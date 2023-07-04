1f02d594ce209e21426c6952c55e604288b6a965
C70Schwager <cyrillgoesweblop@protonmail.com>
2023-07-04 11:10:29 +0200

    popop

--- README.md
+++
@@ -1,3 +0,0 @@
-# my-first-php
-
-brauche **Hilfe**
--- header.php
+++
@@ -1,6 +0,0 @@
-<header class="header-main">
-    <nav class="main-nav">
-        <a href="./index.php">home</a>
-        <a href="./about.php">about</a>
-    </nav>
-</header>
